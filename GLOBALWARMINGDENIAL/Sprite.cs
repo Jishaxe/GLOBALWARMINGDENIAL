7c685b55820cae1c1cfdca612cfcde5c4b6356e6
Josh Lee <josh@joshbe.me>
2017-02-17 23:35:50 +0000

    Basic sprite stuff and a bit of programmer art

---
+++ GLOBALWARMINGDENIAL/Sprite.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLOBALWARMINGDENIAL
+{
+    // Basic sprite class, please extend this.
+    class Sprite
+    {
+        public Vector2 position = new Vector2();
+        public Texture2D texture;
+        
+        public void Draw (SpriteBatch batch)
+        {
+            batch.Draw(texture, position, Color.White);
+        }
+
+        public void Update ()
+        {
+
+        }
+    }
+}
